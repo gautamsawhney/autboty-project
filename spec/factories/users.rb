ed87970a0e0d525e1250a39f99233e294e86043c
gautamsawhney <gsawhney21@gmail.com>
2016-06-19 13:24:05 -0400

    Added Request Specs for Login

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,44 @@
 FactoryGirl.define do
-  factory :user do
-    name "MyString"
+
+	sequence(:email) { |n| "user#{n}@example.com" }
+	
+	factory :user do
+    email
+    password '12345678'
+    password_confirmation '12345678'
+    #roles :admin
   end
+
+  #roles
+factory :associate_software_engineer, class: User do
+                    email
+                    password '12345678'
+                    password_confirmation '12345678'
+                    roles :associate_software_engineer
+                  end
+factory :software_engineer, class: User do
+                    email
+                    password '12345678'
+                    password_confirmation '12345678'
+                    roles :software_engineer
+                  end
+factory :technical_lead, class: User do
+                    email
+                    password '12345678'
+                    password_confirmation '12345678'
+                    roles :technical_lead
+                  end
+factory :business_owner, class: User do
+                    email
+                    password '12345678'
+                    password_confirmation '12345678'
+                    roles :business_owner
+                  end
+factory :business_admin, class: User do
+                    email
+                    password '12345678'
+                    password_confirmation '12345678'
+                    roles :business_admin
+                  end
+  
 end
