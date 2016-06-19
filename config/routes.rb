3c4ab667f90a533f4fd71576b0c91769ece3609d
gautamsawhney <gsawhney21@gmail.com>
2016-06-19 13:25:12 -0400

    Generated API Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
 
   devise_for :users
-	# Engines
+  # Engines
 
-	# root
+  # root
 
-	# Api
-
-	# Api End
+  # Api
+  namespace :api do 
+    namespace :v1 do 
+      resources :locations do
+      end
+      resources :comments do
+      end
+    end
+  end
+  # Api End
 
   #Devise Routes For Api
-      devise_scope :user do
+  devise_scope :user do
     delete '/logout', :to => 'api/v1/users/sessions#destroy'
     post '/login', :to => 'api/v1/users/sessions#create'
     post '/signup', :to => 'api/v1/users/registrations#create'
