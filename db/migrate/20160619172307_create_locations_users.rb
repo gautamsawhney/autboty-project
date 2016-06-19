de62b0b0ddce21eec5d7c8f28b6dd52b1576c20e
gautamsawhney <gsawhney21@gmail.com>
2016-06-19 13:23:07 -0400

    Added User model

---
+++ db/migrate/20160619172307_create_locations_users.rb
@@ -0,0 +1,11 @@
+class CreateLocationsUsers < ActiveRecord::Migration
+  def change
+    create_table :locations_users, :id => false do |t|
+      t.references :location, :user
+    end
+
+    add_index :locations_users, [:location_id, :user_id],
+      name: "locations_users_index",
+      unique: true
+  end
+end
